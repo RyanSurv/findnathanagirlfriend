bcfdcf73cd0b940254cd0b3b750a450df34db946
Ryan C Whims-Survilo <rcsurvilo@gmail.com>
2021-09-03 00:58:41 -0500

    ryan footer easter egg

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-console.log(`
-
-                                                                                     
+console.log(`                                                                                     
 _____     _   _              _                     _ _ _              ___ ___ _     
 |   | |___| |_| |_ ___ ___   |_|___    _____ _ _   | |_| |   _____ _ _|  _|  _|_|___ 
 | | | | .'|  _|   | .'|   |  | |_ -|  |     | | |  | | | |  |     | | |  _|  _| |   |
@@ -10,14 +8,26 @@ _____     _   _              _                     _ _ _              ___ ___ _
 `);
 
 const heart = document.getElementById('heart');
-const interested = document.getElementById('interested');
+
+const ryan = document.getElementById('ryan');
+const ryanH4 = document.getElementById('ryanH4');
+
+const originalH1 = document.getElementById('originalH1');
+const newH1 = document.getElementById('newH1');
+
 let heartClicked = 0;
 
 heart.addEventListener('click', () => {
+  if (heartClicked > 5) return;
+
   heartClicked++;
 
-  if (heartClicked > 9) {
-    console.log('here');
-    interested.style.display = 'block';
+  if (heartClicked > 4) {
+    originalH1.style.display = 'none';
+    newH1.style.display = 'block';
   }
+});
+
+ryan.addEventListener('click', () => {
+  ryanH4.style.display = 'block';
 });
